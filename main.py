945075c75cb5c27f579272beb62db3b913f3f374
Janani Manikandan <janani.m2022cce@sece.ac.in>
2024-06-05 06:36:02 +0530

    website/url

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ from nltk.corpus import stopwords
 import string
 import nltk
 from nltk.stem.porter import PorterStemmer
+from API import get_prediction
+
+# Download NLTK data
+nltk.download('punkt')
+nltk.download('stopwords')
 
 ps = PorterStemmer()
 
+# CSS for styling the app
 css = """
 <style>
 body {
@@ -74,18 +80,33 @@ def transform_text(text):
 
     return " ".join(y)
 
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
+# Load models
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model1 = pickle.load(open('model.pkl', 'rb'))
 
+# Email/SMS Spam Detection
 st.title("Email/SMS Spam Detection")
 
 input_sms = st.text_area("Enter the message")
 
-if st.button('Predict'):
+if st.button('Predict SMS'):
     transformed_sms = transform_text(input_sms)
     vector_input = tfidf.transform([transformed_sms])
-    result = model.predict(vector_input)[0]
+    result = model1.predict(vector_input)[0]
     if result == 1:
         st.header("Spam")
     else:
         st.header("It is safe to proceed")
+
+# Website Spam Detection
+st.title("Website Spam Detection")
+
+input_url = st.text_area("Enter the URL")
+
+if st.button('Predict URL'):
+    model_path = 'model2.pkl'  # Path to your joblib-saved model
+    result, percentage = get_prediction(input_url, model_path)
+    if result == 'bad':
+        st.header(f"Spam - There is {percentage}% chance of being malicious")
+    else:
+        st.header(f"It is safe to proceed - There is {percentage}% chance of being malicious")
